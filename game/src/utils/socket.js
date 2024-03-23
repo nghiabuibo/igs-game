b2d84f1232c5d0d8c3828aa9f94fe6592481e39f
Nghia Bui <nhunghia.1992@gmail.com>
2024-03-23 16:52:11 +0700

    minor update socket path

--- game/src/utils/socket.js
+++ game/src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client"
 
 const socket = io(process.env.REACT_APP_SOCKET_URL, {
     autoConnect: false,
-    path: process.env.REACT_APP_SOCKET_PATH + 'socket.io'
+    path: `${process.env.REACT_APP_SOCKET_PATH || ''}/socket.io`
 })
 
 export default socket
